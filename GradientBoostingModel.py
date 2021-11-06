07508478f383301d78d16580357f0f0ab916ad56
Marshal Krakauer <marshallkrakauer@gmail.com>
2021-11-06 15:29:23 -0700

    added prediction

--- GradientBoostingModel.py
+++ GradientBoostingModel.py
@@ -4,7 +4,7 @@ import random
 from GBCTree import GBCTree
 from GBRTree import GBRTree
 from AbstractDecisionTree import print_breadth_first, get_dataframe
-
+import numpy as np
 
 class GBCModel:
 
@@ -79,14 +79,54 @@ class GBCModel:
 
         return model
 
+    def predict(self, individual_value, cutoff = 0.5):
+        if not self.tree_list:
+            raise AttributeError("Model has not been fit, so predictions can be made.")
+
+        prediction_li = []
+
+        for tree in self.tree_list:
+            if self.is_classification:
+                prediction_li.append(tree.predict_proba(individual_value))
+            else:
+                prediction_li.append(tree.predict(individual_value))
+
+        avg_prediction = np.mean(prediction_li)
+
+        # For a classification problem, return 1 or 0 depending on value and cutoff
+        if self.is_classification:
+            if avg_prediction >= cutoff:
+                avg_prediction = 1
+            else:
+                avg_prediction = 0
+
+        return avg_prediction
+
+    def predict_proba(self, individual_value):
+        if not self.is_classification:
+            raise AttributeError("predict_proba is not available for regression. Please use .predict instead")
+
+        else:
+            prediction_li = []
+
+            for tree in self.tree_list:
+                prediction_li.append(tree.predict_proba(individual_value))
+
+        return np.mean(prediction_li)
+
 if __name__ == '__main__':
-    is_classification_0 = False
-    print_trees = True
+    is_classification_0 = True
+    print_trees = False
 
     df, individual_val, true_value = get_dataframe(is_classification_0)
     gbm = GBCModel(dataframe=df, y_col='y', max_depth=3, gamma = 2,
                    num_trees=3, random_seed=777, is_classification=is_classification_0)
     gbm.make_gbm()
+
+    print('true value:', true_value, 'predicted value:', gbm.predict(individual_val))
+
+    if is_classification_0:
+        print('true value:', true_value, 'predicted value:', gbm.predict_proba(individual_val))
 
     if print_trees:
         for idx, gbm_tree in enumerate(gbm.tree_list):
